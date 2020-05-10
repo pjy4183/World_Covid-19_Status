2f63709f153ae28ce610c0d511ef47a7cdd88605
pjy4183 <pjy4183@gmail.com>
2020-05-10 14:34:58 -0500

    Update

--- noti.py
+++ noti.py
@@ -1,8 +1,8 @@
 from config import TELEGRAM_TOKEN2, CHAT_ID2
 import requests as rq
-import telegram
+import telegram 
 
-bot = telegram.Bot(token = TELEGRAM_TOKEN2)
+bot = telegram.Bot(token=TELEGRAM_TOKEN2)
 
 def send(t):
-    bot.sendMessage(CHAT_ID2, t, parse_mode=telegram.ParseMode.HTML)
+  bot.sendMessage(CHAT_ID2, t, parse_mode=telegram.ParseMode.HTML)
