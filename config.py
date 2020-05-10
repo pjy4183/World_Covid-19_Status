1b19ea7b697c2b0b95e6a1fc94b400aa45f19d15
pjy4183 <pjy4183@gmail.com>
2020-05-10 14:12:00 -0500

    Update

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
-TELEGRAM_TOKEN2 = os.environ.get('TELEGRAM_TOKEN2','')
-CHAT_ID2 = os.environ.get('CHAT_ID2','')
+TELEGRAM_TOKEN2 = os.environ.get('TELEGRAM_TOKEN2', '')
+CHAT_ID2 = os.environ.get('CHAT_ID2', '')
 
 if not TELEGRAM_TOKEN2 or not CHAT_ID2:
-    raise Exception('TELEGRAM_TOKEN or CHAT_ID needs to be checked')
+  raise Exception('TELEGRAM_TOKEN, CHAT_ID 확인필요')
 
 if __name__ == "__main__":
   print(TELEGRAM_TOKEN2)
